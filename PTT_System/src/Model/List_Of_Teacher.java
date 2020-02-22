0c8140a9fd3a2b5ba4918060cacc7ee8aebd8aee
“JunHao <2414816h@student.gla.ac.uk>
2020-02-22 13:53:13 +0000

    start PTT object..

---
+++ PTT_System/src/Model/List_Of_Teacher.java
@@ -0,0 +1,5 @@
+package Model;
+
+public class List_Of_Teacher {
+
+}
---
+++ PTT_System/src/Model/Request.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class Request {
+	private int NumOfTeacher;
+	private int flag = 0; //if the flag is 0, this means this request haven't been checked by PTT director
+							// 1 means accepted
+							// 2 means rejected
+
+}
